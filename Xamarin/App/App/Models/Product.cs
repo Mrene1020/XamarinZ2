ada6f0ac3f095c0fe58079d90cb4e59fad08844b
Your Name <you@example.com>
2019-05-16 20:00:21 -0400

    con verificacion a internet y la api web

---
+++ Xamarin/App/App/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace App.Models
+{
+    class Product
+    {
+
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+
+}
+    
+
